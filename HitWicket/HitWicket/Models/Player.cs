af274160f0253890ff52bd2521f4d99d3693e2c3
sandeep eti <sandeep.eati1106@gmail.com>
2018-11-02 17:46:39 +0530

    Added skill type to player attributes

--- HitWicket/HitWicket/Models/Player.cs
+++ HitWicket/HitWicket/Models/Player.cs
@@ -6,16 +6,49 @@ using System.Threading.Tasks;
 
 namespace HitWicket.Models
 {
+    public enum SkillLevel {
+        NIL = 1,
+        Hopeful,
+        Useful,
+        Moderate,
+        Average,
+        Reliable,
+        Accomplished,
+        Remarkable,
+        Brilliant,
+        Exemplary,
+        Prodigious,
+        Fantastic,
+        Magnificent,
+        Masterful,
+        Supreme,
+        Magical,
+        Legendary,
+        Wonderful,
+        Demigod,
+        Titan
+    }
     public class Player
     {
         public long ManagerId { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
         public int Age { get; set; }
-        public int Fielding { get; set; }
-        public int Wicketkeeping { get; set; }
-        public int Batting { get; set; }
-        public int Bowling { get; set; }
+        public Skill Fielding { get; set; }
+        public Skill Wicketkeeping { get; set; }
+        public Skill Batting { get; set; }
+        public Skill Bowling { get; set; }
         public string Style { get; set; }
+    }
+
+    public class Skill
+    {
+        public Skill(int value)
+        {
+            SkillValue = value;
+            SkillLevel = Enum.GetName(typeof(SkillLevel), SkillValue / 5).ToString();
+        }
+        public int SkillValue { get; set; }
+        public string SkillLevel { get; set; }
     }
 }

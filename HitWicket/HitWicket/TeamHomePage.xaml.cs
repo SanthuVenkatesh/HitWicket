c62f352639f9ac6e58b05740559fb4822e96e3f1
sandeep eti <sandeep.eati1106@gmail.com>
2018-11-02 18:00:54 +0530

    Merge branch 'Venkatesh' of https://github.com/SanthuVenkatesh/HitWicket
    # Conflicts:
    #	HitWicket/HitWicket/HitWicket.csproj

---
+++ HitWicket/HitWicket/TeamHomePage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HitWicket
+{
+    /// <summary>
+    /// Interaction logic for TeamHomePage.xaml
+    /// </summary>
+    public partial class TeamHomePage : Window
+    {
+        public TeamHomePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
